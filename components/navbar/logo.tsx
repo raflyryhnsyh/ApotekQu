ff1cba7c1285c942fd17b4cca3e69f0fda41e102
raflyryhsnyh <rayhansyahrafly@gmail.com>
2025-07-28 10:58:02 +0700

    fix(Logo): update logo size for better alignment in navbar

--- components/navbar/logo.tsx
+++ components/navbar/logo.tsx
@@ -2,7 +2,7 @@ export const Logo = () => (
     <div className="flex items-center space-x-2">
         <img
             src="/logo.png"
-            className="w-10 h-10 contain-content"
+            className="w-4 h-4 contain-content"
         />
         <span className="text-xl font-bold">Apotek Qu</span>
     </div>
